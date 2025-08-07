a911db195016bc118da81e062ac1860b73ce5606
Jose Leonardo Gonzalez Valadez <jleonardo18071999@gmail.com>
2025-08-07 16:34:05 -0600

    finish first deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
                 Contáctame
               </a>
               <a
-                href="/CV_Jose_Leonardo_Gonzalez_Valadez.pdf"
+                href="/CV_Jose_Leonardo_Gonzalez_Valadez_2025_jul.pdf"
                 className="inline-flex items-center justify-center border border-indigo-600 text-indigo-400 px-6 py-2 rounded-full hover:bg-indigo-50 transition text-sm sm:text-base text-center"
                 target="_blank"
                 rel="noopener noreferrer"
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,27 +6,58 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
-      title: "Pagina web DYL Veterinaria",
-      description:
-        "Desarrollada con React y Boostrap CSS, esta web se centra en la atención de pacientes de la veterinaria DYL, ofreciendo información sobre los servicios y la atención que se brinda. Además, se incluyen opciones para realizar reservas de citas médicas mediante whatsapp, así como un formulario para contactar con el equipo de atención al cliente. La página se adapta a diferentes dispositivos y tiene un diseño moderno y limpio, lo que facilita la navegación y la experiencia del usuario, al igual que la creacion y administracion de su blog en blogger.",
-      videoSrc: "/src/assets/media/video-dyl.mp4",
-      poster: "/src/assets/img/project-dyl.webp", // Optional poster image
-    },
-    {
-      id: 2,
-      title: "Pagina web de JEG Dev Studio",
-      description:
-        "Pagina web desarrollada con React, Next.JS y Bootstrap CSS, enfocada en mostrar los servicios de desarrollo web y aplicaciones móviles de JEG Dev Studio. Incluye un portafolio interactivo, testimonios de clientes y un formulario de contacto para consultas. El diseño es responsivo y moderno, optimizado para una excelente experiencia de usuario en todos los dispositivos. Al igual que la administracion de su blog en blogger.",
-      videoSrc: "/src/assets/media/video-jeg.mp4", // Reemplaza con la URL de tu video
-      poster: "/src/assets/img/project-jeg.webp",
-    },
-    {
-      id: 3,
       title: "Proximamente - Rediseño de la web de ETN Turistar Lujo",
       description:
         "Se esta rediseñando actualmente la web de ETN Turistar Lujo y de todas sus filiales como son Costa Line, Autovias y Rapidos de Colima, con una nueva interfaz de usuario y una mejor experiencia de usuario. Utilizando Web Components y reestructurando para que se tenga un mayor alcance SEO y GEO.",
       videoSrc: "", // Reemplaza con la URL de tu video
       poster: "/src/assets/img/project-etn.webp",
+      // Este proyecto no tiene links para demostrar que el espacio no se muestra.
+    },
+    {
+      id: 2,
+      title: "App CRM Progela",
+      description:
+        "Desarrollo de la aplicación CRM para Progela, una empresa dedicada a la venta de medicamentos y suplementos. Esta aplicación permite gestionar clientes, pedidos y productos de manera eficiente, mejorando la productividad y la organización del equipo de ventas.",
+      videoSrc: "/src/assets/media/video-progela.mp4", // Reemplaza con la URL de tu video
+      poster: "/src/assets/img/project-progela.webp",
+      links: [
+        { label: "Ver Documento", url: "/Manual-CRM-Progela-V1.pdf" },
+        {
+          label: "Ver en GitHub",
+          url: "https://github.com/GonzalezValadez18/CrmProgela",
+        }, // URL de ejemplo
+      ],
+    },
+    {
+      id: 3,
+      title: "Pagina web DYL Veterinaria",
+      description:
+        "Desarrollada con React y Boostrap CSS, esta web se centra en la atención de pacientes de la veterinaria DYL, ofreciendo información sobre los servicios y la atención que se brinda. Además, se incluyen opciones para realizar reservas de citas médicas mediante whatsapp, así como un formulario para contactar con el equipo de atención al cliente. La página se adapta a diferentes dispositivos y tiene un diseño moderno y limpio, lo que facilita la navegación y la experiencia del usuario, al igual que la creacion y administracion de su blog en blogger.",
+      videoSrc: "/src/assets/media/video-dyl.mp4",
+      poster: "/src/assets/img/project-dyl.webp", // Optional poster image
+      links: [
+        { label: "Ver en Vivo", url: "https://veterinaria-dyl.vercel.app/" },
+        {
+          label: "Ver Blog",
+          url: "https://dyl-cuidando-huellitas.blogspot.com/",
+        },
+        {
+          label: "Ver App",
+          url: "/Mi-mejor-amigo.pdf",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Pagina web de JEG Dev Studio",
+      description:
+        "Pagina web desarrollada con React, Next.JS y Bootstrap CSS, enfocada en mostrar los servicios de desarrollo web y aplicaciones móviles de JEG Dev Studio. Incluye un portafolio interactivo, testimonios de clientes y un formulario de contacto para consultas. El diseño es responsivo y moderno, optimizado para una excelente experiencia de usuario en todos los dispositivos. Al igual que la administracion de su blog en blogger.",
+      videoSrc: "/src/assets/media/video-jeg.mp4",
+      poster: "/src/assets/img/project-jeg.webp",
+      links: [
+        { label: "Ver en Vivo", url: "https://www.jegdevstudios.com/" },
+        { label: "Ver Blog", url: "https://blog.jegdevstudios.com/" },
+      ],
     },
     // Puedes agregar más proyectos aquí
   ];
